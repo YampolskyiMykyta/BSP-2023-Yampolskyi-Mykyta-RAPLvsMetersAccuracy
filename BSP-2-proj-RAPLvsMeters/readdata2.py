5918750731659fe0498c2cf9321e3ff29d6df6b8
YampolskyiMykyta <114622408+YampolskyiMykyta@users.noreply.github.com>
2023-03-07 21:28:14 +0100

    Update readdate2 file (corrections and comments)

--- BSP-2-proj-RAPLvsMeters/readdata2.py
+++ BSP-2-proj-RAPLvsMeters/readdata2.py
@@ -2,122 +2,167 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import datetime
 
-mass_threads= [1,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36,38,40,42,44,46,48,50,52,54,56,58,60,62,64]
+# Global variables with data from tables
+mass_threads= [1]+list(range(2, 66, 2))
 
+# DataMeters and DataMetersTime
 dm = pd.read_csv('dataMeters.csv', sep=';', usecols=['Reading', 'Sample', 'Start Time', 'Duration', 'Max Time', 'Max', 'Average', 'Min Time', 'Min', 'Description', 'Stop Time'])
 dmt = pd.read_csv('dataMetersTime.csv', sep=',', usecols=['Configurations', 'Start Time', 'Stop Time'])
 
+# DataMeters2 and DataMetersTime2
 dm2 = pd.read_csv('dataMeters2.csv', sep=';', usecols=['Reading', 'Sample', 'Start Time', 'Duration', 'Max Time', 'Max', 'Average', 'Min Time', 'Min', 'Description', 'Stop Time'])
 dmt2 = pd.read_csv('dataMetersTime2.csv', sep=',', usecols=['Configurations', 'Start Time', 'Stop Time'])
 
+# DataRAPL
 dr = pd.read_csv('dataRAPL.csv', sep=',', usecols=['version', 'n-workers', 'n-secondary-workers', 'n-reservations', 'n-relations', 'n-queries', 'password-work-factor', 'Energy Cores', 'Energy Ram', 'Energy Gpu', 'Energy Pkg', 'Time (perf) (s)', 'Time (exec) (s)', 'Ratio time (%)', 'Power_Cores', 'Power_Ram', 'Power_Gpu', 'Power_Pkg'])
 
-def get_zip_startend_time(source,ind):
+
+def get_zip_startend_time(source, ind):
+    """ Returns the time frames from the source table
+
+    @:param source: The used table
+    @:type source: pandas type
+    @:param ind: The index of the part with time
+    @:type ind: int
+    @:returns: A zipped set with start and end time
+    @:rtype: set
+    """
     starts = [i.split(" ")[ind] for i in source["Start Time"]]
     ends = starts[1:] + [(source["Stop Time"][-1:].values[0]).split(" ")[ind]]
-    return zip(starts,ends)
+    return zip(starts, ends)
 
-def get_data_for_15(a, b,source_dm):
-    id_dict, energy_ress,mega_b = 1, {i: [] for i in mass_threads},b
+
+def get_data_for_15(a, b, source_dm):
+    """ Calculates, with the provided formulas, averages for the 1...64 threads
+    in first 15 blocks and then returns the list with them
+
+    formula: Energy[n] =  C9[n] x U x C5[n]
+
+    Where:
+    C9 is the Average
+    U is the line voltage, which was equal to 237.2 Volts
+    C5[n] is the duration
+
+    after getting the list, divide its sum by 15 (blocks)
+
+    @:param a: The list with time frames to be used
+    @:type a: set
+    @:param b: The list with time frames to be checked
+    @:type b: int
+    @:param source_dm: A DataMeters file to be used
+    @:type source_dm: pandas type
+    @:returns: a list with averages for the 1...64 threads in first fifteen blocks
+    @:rtype: list
+    """
+    id_dict, energy_ress, mega_b, cc = 1, {i: [] for i in mass_threads}, b, 1
     for (start_a, end_a) in a:
-        #print(">>",id_dict, " <<")
-        #print("    ", start_a," --- ", end_a)
+        # print(">>> ",cc," <<< ") if id_dict ==1 else None
+        # print(">>",id_dict, " <<")
+        # print("    ", start_a," --- ", end_a)
         for (start_b, end_b) in b:
             if start_a <= start_b and end_b <= end_a:
-                energy_ress[id_dict] +=[float(str(source_dm["Average"][mega_b.index((start_b, end_b))]).replace(',','.'))*237.2 * float(str(source_dm["Duration"][mega_b.index((start_b, end_b))]).split(":")[2].replace(',','.'))]
-                #print(float(str(source_dm["Duration"][b.index((start_b, end_b))]).split(":")[2].replace(',','.')))
-                #print("    "*2,start_b, " -- ", end_b, " ~ ", float(str(source_dm["Average"][b.index((start_b, end_b))]).replace(',','.')), " * ",237.2, " * ",  float(str(source_dm["Duration"][b.index((start_b, end_b))]).split(":")[2].replace(',','.')))
-            if start_b>start_a and end_b > end_a:
+                energy_ress[id_dict] +=[float(str(source_dm["Average"][mega_b.index((start_b, end_b))]).replace(',','.')) * 237.2 * float(str(source_dm["Duration"][mega_b.index((start_b, end_b))]).split(":")[2].replace(',','.'))]
+                # print(float(str(source_dm["Duration"][b.index((start_b, end_b))]).split(":")[2].replace(',','.')))
+                # print("    " * 2, start_b, " -- ", end_b, " ~ ", float(str(source_dm["Average"][mega_b.index((start_b, end_b))]).replace(',', '.')), " * ", 237.2, " * ", float(str(source_dm["Duration"][mega_b.index((start_b, end_b))]).split(":")[2].replace(',', '.')))
+            if start_b > start_a and end_b > end_a:
                 b = b[b.index((start_b, end_b))-1:]
                 break
-        id_dict+=1 if id_dict ==1 else 2
-        if id_dict == 66: id_dict = 1
-    return [sum(energy_ress[i])/len(energy_ress[i]) for i in mass_threads]
-
-def get_time_in_time(dm,dmt):
-    a_dt_1 = [(datetime.datetime.strptime(start, '%H:%M:%S'), datetime.datetime.strptime(end, '%H:%M:%S')) for start, end in get_zip_startend_time(dmt,4)]
-    b_dt_1 = [(datetime.datetime.strptime(start, '%H:%M:%S,%f'),datetime.datetime.strptime(end, '%H:%M:%S,%f')) for start, end in get_zip_startend_time(dm,1)]
-    return (a_dt_1,b_dt_1)
+        id_dict += 1 if id_dict == 1 else 2
+        if id_dict == 66: id_dict = 1; cc += 1
+    # return [sum(energy_ress[i])/len(energy_ress[i]) for i in mass_threads]
+    return [sum(energy_ress[i])/15 for i in mass_threads]
 
 
-def get_res_data_rapl(dr):
-    return [i + j + k + t for (i, j, k, t) in zip(dr['Energy Cores'], dr['Energy Ram'], dr['Energy Gpu'], dr['Energy Pkg'])]
+def get_time_in_time(dm_tu, dmt_tu):
+    """ Returns the set with time frame in time format
 
-def calc_avar(mass,source):
-    for j in range(33):mass.append(sum(list(source[j::33])) /(len(source)/ 33))
+    @:param dm_tu: The used DataMeters table
+    @:type dm_tu: pandas type
+    @:param dmt_tu: The used DataMetersTime table
+    @:type dmt_tu: pandas type
+    @:returns: A set with start and end time in time format
+    @:rtype: set
+    """
+    a_dt = [(datetime.datetime.strptime(start, '%H:%M:%S'), datetime.datetime.strptime(end, '%H:%M:%S')) for start, end in get_zip_startend_time(dmt_tu,4)]
+    b_dt = [(datetime.datetime.strptime(start, '%H:%M:%S,%f'),datetime.datetime.strptime(end, '%H:%M:%S,%f')) for start, end in get_zip_startend_time(dm_tu,1)]
+    return (a_dt, b_dt)
 
-(a_dt_1,b_dt_1) = get_time_in_time(dm,dmt)
-(a_dt_2,b_dt_2) = get_time_in_time(dm2,dmt2)
 
-mega_mass_with_mega_data ={(i+1):(get_data_for_15(a_dt_1[495*i:495+495*i], b_dt_1, dm) ,get_data_for_15(a_dt_2[495 * i:495 + 495 * i], b_dt_2, dm2))  for i in range(5)}
+def get_res_data_rapl(dr_tu):
+    """ Returns a list with all Energy consumption related columns row sums
+
+    @:param dr_tu: The used DataRAPL table
+    @:type dr_tu: pandas type
+    @:returns: A list with sums
+    @:rtype: list
+    """
+    return [i + j + k + t for (i, j, k, t) in zip(dr_tu['Energy Cores'], dr_tu['Energy Ram'], dr_tu['Energy Gpu'], dr_tu['Energy Pkg'])]
+
+
+def calc_avar(mass, source):
+    """ Adds, to the passed to the function list, averages for the 1...64 threads
+
+    @:param mass: The list to be changed
+    @:type mass: list
+    @:param source: The list with data to be used
+    @:type source: list
+    """
+    for j in range(33): mass.append(sum(list(source[j::33])) / ((len(source) / 33)))
+
+
+# CALLING FUNCTIONS:
+
+# getting time frames in time format for DatMeters and DatMetersTime tables
+(a_dt_1,b_dt_1) = get_time_in_time(dm, dmt)
+(a_dt_2,b_dt_2) = get_time_in_time(dm2, dmt2)
+
+# getting the averages for the 15 blocks for two files and
+# saving them into a dictionary with key saying if it is original(1) or txact(>1)
+mega_mass_with_mega_data = {(i+1): (get_data_for_15(a_dt_1[495*i:495+495*i], b_dt_1, dm), get_data_for_15(a_dt_2[495 * i:495 + 495 * i], b_dt_2, dm2)) for i in range(5)}
+
+# because we need average for 30 blocks just summing respectful results and divide by 2 (15+15 = 30 blocks)
 newMMWMD = {}
+for j in range(1, 6):
+    newMMWMD[j] = [(mega_mass_with_mega_data[j][0][i] + mega_mass_with_mega_data[j][1][i])/2 for i in range(33)]
 
-
-for j in range(1,6):
-    sumpi = [(mega_mass_with_mega_data[j][0][i] + mega_mass_with_mega_data[j][1][i])/2 for i in range(33)]
-    newMMWMD[j] =sumpi
-
+# getting data from RAPL and calculating averages for all 30 blocks (5)
 mass_with_ress = get_res_data_rapl(dr)
-mass_data_rapl_or =[]
-mass_data_rapl_tx1 =[]
-mass_data_rapl_tx2 =[]
-mass_data_rapl_tx3 =[]
-mass_data_rapl_tx4 =[]
 
+# print(len(mass_with_ress))  #4950
+mass_data_rapl_or =[]  # original
+mass_data_rapl_tx1 =[]  # txact
+mass_data_rapl_tx2 =[]  # txact
+mass_data_rapl_tx3 =[]  # txact
+mass_data_rapl_tx4 =[]  # txact
 calc_avar(mass_data_rapl_or,mass_with_ress[0:990])
 calc_avar(mass_data_rapl_tx1,mass_with_ress[990:1980])
 calc_avar(mass_data_rapl_tx2,mass_with_ress[1980:2970])
 calc_avar(mass_data_rapl_tx3,mass_with_ress[2970:3960])
 calc_avar(mass_data_rapl_tx4,mass_with_ress[3960:4950])
 
-yl = [1]+list(range(2,66,2))
-
+# Creating plots with all collected data
 figure, axis = plt.subplots(2)
-#
 
-# axis[0].plot(yl, mega_mass_with_mega_data[1][0],"b",label= "origin")
-# axis[0].plot(yl, mega_mass_with_mega_data[2][0],"r",label= "txact")
-# axis[0].plot(yl, mega_mass_with_mega_data[3][0],"r",label= "txact")
-# axis[0].plot(yl, mega_mass_with_mega_data[4][0],"r",label= "txact")
-# axis[0].plot(yl, mega_mass_with_mega_data[5][0],"r",label= "txact")
-# axis[0].set_title("Meters data (origin blue) energy consumtion 15")
-# axis[0].set_xlabel('1-64 threads')
-# axis[0].set_ylabel('energy consumption (J)')
-#
-
-# axis[1].plot(yl, mega_mass_with_mega_data[1][1],"b",label= "origin")
-# axis[1].plot(yl, mega_mass_with_mega_data[2][1],"r",label= "txact")
-# axis[1].plot(yl, mega_mass_with_mega_data[3][1],"r",label= "txact")
-# axis[1].plot(yl, mega_mass_with_mega_data[4][1],"r",label= "txact")
-# axis[1].plot(yl, mega_mass_with_mega_data[5][1],"r",label= "txact")
-# axis[1].set_title("Meters data (origin blue) energy consumtion 15")
-# axis[1].set_xlabel('1-64 threads')
-# axis[1].set_ylabel('energy consumption (J)')
-
-# For Cosine Function
-
-axis[0].plot(yl, newMMWMD[1],"b",label= "origin")
-axis[0].plot(yl, newMMWMD[2],"r",label= "txact")
-axis[0].plot(yl, newMMWMD[3],"r",label= "txact")
-axis[0].plot(yl, newMMWMD[4],"r",label= "txact")
-axis[0].plot(yl, newMMWMD[5],"r",label= "txact")
+# METERS PLOT (TOP)
+axis[0].plot(mass_threads, newMMWMD[1], "b", label= "origin")
+axis[0].plot(mass_threads, newMMWMD[2], "r", label= "txact")
+axis[0].plot(mass_threads, newMMWMD[3], "r", label= "txact")
+axis[0].plot(mass_threads, newMMWMD[4], "r", label= "txact")
+axis[0].plot(mass_threads, newMMWMD[5], "r", label= "txact")
 axis[0].set_title("Meters data (origin blue) energy consumtion 30")
-#axis[0].set_xlabel('1-64 threads')
+# axis[0].set_xlabel('1-64 threads')
 axis[0].set_ylabel('energy consumption (J)')
 axis[0].legend()
 
-axis[1].plot(yl, mass_data_rapl_or,"m",label= "origin")
-axis[1].plot(yl, mass_data_rapl_tx1,"c",label= "txact")
-axis[1].plot(yl, mass_data_rapl_tx2,"c",label= "txact")
-axis[1].plot(yl, mass_data_rapl_tx3,"c",label= "txact")
-axis[1].plot(yl, mass_data_rapl_tx4,"c",label= "txact")
-#axis[1].set_title("RAPL data (origin magenta) energy consumtion 30")
+# RAPL PLOT (BOTTOM)
+axis[1].plot(mass_threads, mass_data_rapl_or, "m", label= "origin")
+axis[1].plot(mass_threads, mass_data_rapl_tx1, "c", label= "txact")
+axis[1].plot(mass_threads, mass_data_rapl_tx2, "c", label= "txact")
+axis[1].plot(mass_threads, mass_data_rapl_tx3, "c", label= "txact")
+axis[1].plot(mass_threads, mass_data_rapl_tx4, "c", label= "txact")
+# axis[1].set_title("RAPL data (origin magenta) energy consumtion 30")
 axis[1].set_xlabel('RAPL data (origin magenta) energy consumtion 30')
 axis[1].set_ylabel('energy consumption (J)')
-# Combine all the operations and display
+
 axis[1].legend()
 plt.show()
-
-
-
-
